182f32ceb23d7605b5518ed065bd0974ca404889
Jonas Wallmann <jonas.wallmann@code.berlin>
2022-05-29 22:31:59 +0200

    Initial Commit

---
+++ NumberGuessing/NumberGuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberGuessingApp.swift
+//  NumberGuessing
+//
+//  Created by Jonas Wallmann on 29.05.22.
+//
+
+import SwiftUI
+
+@main
+struct NumberGuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
